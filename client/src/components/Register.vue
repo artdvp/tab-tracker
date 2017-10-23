f8b9f275218a7860ab1450909d91af4f43aa936f
Art dvp <sphinspf@gmail.com>
2017-10-23 21:09:18 +0700

    test Register.vue

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
    <h1>Register</h1>
+   <input 
+        type="email" 
+        name="email"
+        v-model="email"
+        placeholder="email" />
+        <br/>
+    <input 
+        type="password"
+        name="password"
+        v-model="password"
+        placeholder="password" />
+        <br/>
+        <button>Register</button>
   </div>
 </template>
 
@@ -9,7 +22,19 @@ export default {
   name: 'Register',
   data () {
     return {
+      email: '',
+      password: ''
     }
+  },
+  watch: {
+    email (value) {
+      console.log('email has change', value)
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.email = 'hello world'
+    }, 2000)
   }
 }
 </script>
